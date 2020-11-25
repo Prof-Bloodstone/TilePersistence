426b00058ec41f0fb2862d40badeea246e0224b4
Professor Bloodstone <prof.bloodstone+git@gmail.com>
2020-11-25 21:00:06 +0100

    Make it work on pre 1.16.2 Paper where snapshotless PDC was null

--- src/main/kotlin/dev/bloodstone/tilepersistence/TilePersistence.kt
+++ src/main/kotlin/dev/bloodstone/tilepersistence/TilePersistence.kt
@@ -29,14 +29,15 @@ import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.Arrays
 
 public class TilePersistence() : JavaPlugin(), Listener {
     private val NAME_KEY: NamespacedKey = NamespacedKey(this, "name")
     private val LORE_KEY: NamespacedKey = NamespacedKey(this, "lore")
     private val LORE_PDT: PersistentDataType<String, Array<String>> = JsonDataType(Array<String>::class.java)
+    private val hasToUseSnapshot = !doesSnapshotlessBlockStateWork()
 
     override fun onEnable() {
+        PaperLib.suggestPaper(this)
         server.pluginManager.registerEvents(this, this)
     }
 
@@ -46,7 +47,7 @@ public class TilePersistence() : JavaPlugin(), Listener {
     @EventHandler
     fun onBlockPlaceEvent(event: BlockPlaceEvent) {
         val block = event.blockPlaced
-        val blockStateSnapshotResult: BlockStateSnapshotResult = PaperLib.getBlockState(block, false)
+        val blockStateSnapshotResult: BlockStateSnapshotResult = PaperLib.getBlockState(block, hasToUseSnapshot)
         val headItem = event.itemInHand
         val meta = headItem.itemMeta ?: return
         val name = meta.displayName
@@ -55,7 +56,7 @@ public class TilePersistence() : JavaPlugin(), Listener {
         val blockPDC = blockState.persistentDataContainer
         blockPDC.set(NAME_KEY, PersistentDataType.STRING, name)
         if (lore != null) blockPDC.set(LORE_KEY, LORE_PDT, lore.toTypedArray())
-        if (blockStateSnapshotResult.isSnapshot()) blockState.update()
+        if (blockStateSnapshotResult.isSnapshot) blockState.update()
     }
 
     @EventHandler
@@ -70,8 +71,26 @@ public class TilePersistence() : JavaPlugin(), Listener {
             // This shouldn't happen
             val meta = itemstack.itemMeta ?: continue
             if (name != null) meta.setDisplayName(name)
-            if (lore != null) meta.lore = Arrays.asList(*lore)
-            itemstack.setItemMeta(meta)
+            if (lore != null) meta.lore = listOf(*lore)
+            itemstack.itemMeta = meta
         }
+    }
+
+    // Whether current version has not-null PDC when not using snapshot
+    // https://github.com/PaperMC/Paper/issues/3913
+    // This was fixed in early 1.16.2 build - 165
+    private fun doesSnapshotlessBlockStateWork(): Boolean {
+        val fixVersion = listOf(1, 16, 2)
+        val currentVersion = server.bukkitVersion // 1.16.4-R0.1-SNAPSHOT
+            .split('-')[0] // 1.16.4
+            .split('.') // listOf("1", "16", "4")
+            .map { it.toInt() }
+        for ((fix, current) in fixVersion zip currentVersion) {
+            if (fix < current) return true
+            if (fix > current) return false
+            // If equal, check next subversion
+        }
+        // Current version is exactly fix version
+        return true
     }
 }
